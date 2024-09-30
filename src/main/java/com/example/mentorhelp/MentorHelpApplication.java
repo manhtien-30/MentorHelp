0c49bf7d89f8eec57af0b270763d20c3859dbfe0
manhtien-30 <tien310001@gmail.com>
2024-09-30 12:29:59 +0700

    init project

---
+++ src/main/java/com/example/mentorhelp/MentorHelpApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorHelpApplication.class, args);
+    }
+
+}
